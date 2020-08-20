71faf5f9e181a1a50c9f2240a60d51241c7ad4a5
Özgür Özbek <ozgurozbek1@yandex.com>
2020-08-20 21:36:07 +0300

    UI Add
    İşini kolaylaştırdım hadi yine iyisin

--- XoooX/Assets/Scripts/Button.cs
+++ XoooX/Assets/Scripts/Button.cs
@@ -2,118 +2,115 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour
-{
-    
+public class Button : MonoBehaviour {
     private GameObject plane;
-    public Vector3 ofset;
-    
-    
-    void OnMouseDown (){
-        if (plane!=null)
-        {
-            Debug.Log("Illegal move!");
+    private GameObject planet;
+    public Vector3 offset;
+
+    void OnMouseDown () {
+        if (plane != null) {
+            Debug.Log ("Illegal move!");
             return;
         }
         //plane yerleştir hamle yap
-        GameObject planeToBuild = GameMaster.instance.GetplaneBuild();
-        plane= (GameObject)Instantiate(planeToBuild,transform.position + ofset , transform.rotation);
-        changeArray(this.name);
+        GameObject planeToBuild = GameMaster.instance.GetPlaneBuild ();
+        GameObject particle = GameMaster.instance.GetParticle ();
+        plane = (GameObject) Instantiate (planeToBuild, transform.position + offset, transform.rotation);
+        planet = (GameObject) Instantiate (particle, transform.position + offset + new Vector3 (0f, 1f, 0f), transform.rotation);
+        changeArray (this.name);
         GameMaster.instance.MoveNumber++;
+        Destroy(planet, 2f);
     }
-    void changeArray(string ButtonName)
-    {
-        string check=ButtonName;
-        switch (check)
-        {
+    void changeArray (string ButtonName) {
+        string check = ButtonName;
+        switch (check) {
             case "1":
-                GameMaster.instance.Red[0,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Red[0, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "2":
-                GameMaster.instance.Red[1,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Red[1, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "3":
-                GameMaster.instance.Red[2,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
-                GameMaster.instance.Green[0,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Red[2, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Green[0, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "4":
-                GameMaster.instance.Green[1,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Green[1, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "5":
-                GameMaster.instance.Green[2,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Green[2, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "6":
-                GameMaster.instance.Red[3,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Red[3, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "7":
-                GameMaster.instance.Red[4,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Red[4, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "8":
-                GameMaster.instance.Red[5,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
-                GameMaster.instance.Green[3,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Red[5, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Green[3, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "9":
-                GameMaster.instance.Green[4,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Green[4, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "10":
-                GameMaster.instance.Green[5,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Green[5, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "11":
-                GameMaster.instance.Red[6,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
-                GameMaster.instance.Yellow[0,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Red[6, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Yellow[0, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "12":
-                GameMaster.instance.Red[7,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
-                GameMaster.instance.Yellow[1,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Red[7, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Yellow[1, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "13":
-                GameMaster.instance.Red[8,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
-                GameMaster.instance.Green[6,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
-                GameMaster.instance.Yellow[2,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
-                GameMaster.instance.Blue[0,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Red[8, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Green[6, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Yellow[2, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Blue[0, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "14":
-                GameMaster.instance.Green[7,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
-                GameMaster.instance.Blue[1,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Green[7, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Blue[1, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "15":
-                GameMaster.instance.Green[8,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
-                GameMaster.instance.Blue[2,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Green[8, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Blue[2, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "16":
-                GameMaster.instance.Yellow[3,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Yellow[3, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "17":
-                GameMaster.instance.Yellow[4,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Yellow[4, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "18":
-                GameMaster.instance.Yellow[5,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
-                GameMaster.instance.Blue[3,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Yellow[5, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Blue[3, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "19":
-                GameMaster.instance.Blue[4,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Blue[4, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "20":
-                GameMaster.instance.Blue[5,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Blue[5, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "21":
-                GameMaster.instance.Yellow[6,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Yellow[6, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "22":
-                GameMaster.instance.Yellow[7,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Yellow[7, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "23":
-                GameMaster.instance.Yellow[8,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
-                GameMaster.instance.Blue[6,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Yellow[8, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Blue[6, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "24":
-                GameMaster.instance.Blue[7,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Blue[7, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
             case "25":
-                GameMaster.instance.Blue[8,0]=GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
+                GameMaster.instance.Blue[8, 0] = GameMaster.instance.Moves[GameMaster.instance.MoveNumber];
                 break;
-            
-            
+
         }
     }
 }
--- XoooX/Assets/Scripts/GameMaster.cs
+++ XoooX/Assets/Scripts/GameMaster.cs
@@ -2,92 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMaster : MonoBehaviour
-{   public string[] Moves = new string[26] {"X", "O", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X"};
-    public string[,] Red = new string[9,2] {{"","1"},{"","2"},{"","3"},{"","6"},{"","7"},{"","8"},{"","11"},{"","12"},{"","13"}};
-    public string[,] Blue = new string[9,2] {{"","13"},{"","14"},{"","15"},{"","18"},{"","19"},{"","20"},{"","23"},{"","24"},{"","25"}};
-    public string[,] Green = new string[9,2] {{"","3"},{"","4"},{"","5"},{"","8"},{"","9"},{"","10"},{"","13"},{"","14"},{"","15"}};
-    public string[,] Yellow = new string[9,2] {{"","11"},{"","12"},{"","13"},{"","16"},{"","17"},{"","18"},{"","21"},{"","22"},{"","23"}};
+public class GameMaster : MonoBehaviour {
+    public string[] Moves = new string[26] { "X", "O", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X" };
+    public string[, ] Red = new string[9, 2] { { "", "1" }, { "", "2" }, { "", "3" }, { "", "6" }, { "", "7" }, { "", "8" }, { "", "11" }, { "", "12" }, { "", "13" } };
+    public string[, ] Blue = new string[9, 2] { { "", "13" }, { "", "14" }, { "", "15" }, { "", "18" }, { "", "19" }, { "", "20" }, { "", "23" }, { "", "24" }, { "", "25" } };
+    public string[, ] Green = new string[9, 2] { { "", "3" }, { "", "4" }, { "", "5" }, { "", "8" }, { "", "9" }, { "", "10" }, { "", "13" }, { "", "14" }, { "", "15" } };
+    public string[, ] Yellow = new string[9, 2] { { "", "11" }, { "", "12" }, { "", "13" }, { "", "16" }, { "", "17" }, { "", "18" }, { "", "21" }, { "", "22" }, { "", "23" } };
 
+    public int MoveNumber = 0;
+    public int Xcount = 0;
+    public int Ocount = 0;
 
-    public int MoveNumber=0;
-    public int Xcount=0;
-    public int Ocount=0;
-    
-   
     public static GameMaster instance;
-    
-    void Awake(){
-        instance=this;
+
+    void Awake () {
+        instance = this;
     }
+
     public GameObject PlaneXprefab;
     public GameObject PlaneOprefab;
-    
-    
-    void Start(){
+    public GameObject SummonParticle;
+    public GameObject ComboParticle;
+
+    void Start () {
         planeToBuild = PlaneXprefab;
+        particle = SummonParticle;
+        comboParticle = ComboParticle;
     }
-    void Update()
-    {   Xcount=0;
-        Ocount=0;
-         if (Moves[MoveNumber]=="X")
-        {
-            planeToBuild=PlaneXprefab;
+
+    void Update () {
+        Xcount = 0;
+        Ocount = 0;
+        if (Moves[MoveNumber] == "X") {
+            planeToBuild = PlaneXprefab;
+        } else {
+            planeToBuild = PlaneOprefab;
         }
-        else
-        {
-            planeToBuild=PlaneOprefab;
-        }
-        Xcount+=Check(Red,"X");
-        Xcount+=Check(Green,"X");
-        Xcount+=Check(Yellow,"X");
-        Xcount+=Check(Blue,"X");
-        Ocount+=Check(Red,"O");
-        Ocount+=Check(Green,"O");
-        Ocount+=Check(Yellow,"O");
-        Ocount+=Check(Blue,"O");
-        Debug.Log("X= "+Xcount);
-        Debug.Log("O= "+Ocount);
-
-
-
-
-
-
-
-
-
+        Xcount += Check (Red, "X");
+        Xcount += Check (Green, "X");
+        Xcount += Check (Yellow, "X");
+        Xcount += Check (Blue, "X");
+        Ocount += Check (Red, "O");
+        Ocount += Check (Green, "O");
+        Ocount += Check (Yellow, "O");
+        Ocount += Check (Blue, "O");
+        Debug.Log ("X= " + Xcount);
+        Debug.Log ("O= " + Ocount);
     }
-    
+
     private GameObject planeToBuild;
-    
-    public GameObject GetplaneBuild()
-    {
-       return planeToBuild;
-    }   
-    public int Check(string[,] OXO , string Word )
-    {
-        string[,] checker = new string[9,2];
+    public GameObject GetPlaneBuild () {
+        return planeToBuild;
+    }
+
+    private GameObject particle;
+    public GameObject GetParticle () {
+        return particle;
+    }
+
+    private GameObject comboParticle;
+    public GameObject GetComboParticle () {
+        return comboParticle;
+    }
+
+    public int Check (string[, ] OXO, string Word) {
+        string[, ] checker = new string[9, 2];
         checker = OXO;
-        int count=0;
-        
-        if (checker[0,0]==Word && checker[1,0]==Word && checker[2,0]==Word)
-        { count++;}
-        if (checker[0,0]==Word && checker[3,0]==Word && checker[6,0]==Word)
-        { count++;}
-        if (checker[0,0]==Word && checker[4,0]==Word && checker[8,0]==Word)
-        { count++;}
-        if (checker[1,0]==Word && checker[4,0]==Word && checker[7,0]==Word)
-        { count++;}
-        if (checker[2,0]==Word && checker[5,0]==Word && checker[8,0]==Word)
-        { count++;}
-        if (checker[2,0]==Word && checker[4,0]==Word && checker[6,0]==Word)
-        { count++;}
-        if (checker[3,0]==Word && checker[4,0]==Word && checker[5,0]==Word)
-        { count++;}
-        if (checker[6,0]==Word && checker[7,0]==Word && checker[8,0]==Word)
-        { count++;}
+        int count = 0;
+
+        if (checker[0, 0] == Word && checker[1, 0] == Word && checker[2, 0] == Word) { count++; }
+        if (checker[0, 0] == Word && checker[3, 0] == Word && checker[6, 0] == Word) { count++; }
+        if (checker[0, 0] == Word && checker[4, 0] == Word && checker[8, 0] == Word) { count++; }
+        if (checker[1, 0] == Word && checker[4, 0] == Word && checker[7, 0] == Word) { count++; }
+        if (checker[2, 0] == Word && checker[5, 0] == Word && checker[8, 0] == Word) { count++; }
+        if (checker[2, 0] == Word && checker[4, 0] == Word && checker[6, 0] == Word) { count++; }
+        if (checker[3, 0] == Word && checker[4, 0] == Word && checker[5, 0] == Word) { count++; }
+        if (checker[6, 0] == Word && checker[7, 0] == Word && checker[8, 0] == Word) { count++; }
         return count;
     }
-
 }
