9fdf359718ec12afad6d1dd642b6c5c37f5fd727
atilla <atillacoruhlu@hotmail.com>
2020-08-20 19:35:32 +0300

    add new bugs

--- XoooX/Assets/Scripts/GameMaster.cs
+++ XoooX/Assets/Scripts/GameMaster.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {   public string[] Moves = new string[26] {"X", "O", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X"};
-    public string[] Red = new string[9];
-    public string[] Blue = new string[9];
-    public string[] Green = new string[9];
-    public string[] Yellow = new string[9];
+    public string[,] Red = new string[9,2] {{"","1"},{"","2"},{"","3"},{"","6"},{"","7"},{"","8"},{"","11"},{"","12"},{"","13"}};
+    public string[,] Blue = new string[9,2] {{"","13"},{"","14"},{"","15"},{"","18"},{"","19"},{"","20"},{"","23"},{"","24"},{"","25"}};
+    public string[,] Green = new string[9,2] {{"","3"},{"","4"},{"","5"},{"","8"},{"","9"},{"","10"},{"","13"},{"","14"},{"","15"}};
+    public string[,] Yellow = new string[9,2] {{"","11"},{"","12"},{"","13"},{"","16"},{"","17"},{"","18"},{"","21"},{"","22"},{"","23"}};
 
 
     public int MoveNumber=0;
@@ -65,27 +65,27 @@ public class GameMaster : MonoBehaviour
     {
        return planeToBuild;
     }   
-    public int Check(string[] OXO , string Word )
+    public int Check(string[,] OXO , string Word )
     {
-        string[] checker = new string[9];
+        string[,] checker = new string[9,2];
         checker = OXO;
         int count=0;
         
-        if (checker[0]==Word && checker[1]==Word && checker[2]==Word)
+        if (checker[0,0]==Word && checker[1,0]==Word && checker[2,0]==Word)
         { count++;}
-        if (checker[0]==Word && checker[3]==Word && checker[6]==Word)
+        if (checker[0,0]==Word && checker[3,0]==Word && checker[6,0]==Word)
         { count++;}
-        if (checker[0]==Word && checker[4]==Word && checker[8]==Word)
+        if (checker[0,0]==Word && checker[4,0]==Word && checker[8,0]==Word)
         { count++;}
-        if (checker[1]==Word && checker[4]==Word && checker[7]==Word)
+        if (checker[1,0]==Word && checker[4,0]==Word && checker[7,0]==Word)
         { count++;}
-        if (checker[2]==Word && checker[5]==Word && checker[8]==Word)
+        if (checker[2,0]==Word && checker[5,0]==Word && checker[8,0]==Word)
         { count++;}
-        if (checker[2]==Word && checker[4]==Word && checker[6]==Word)
+        if (checker[2,0]==Word && checker[4,0]==Word && checker[6,0]==Word)
         { count++;}
-        if (checker[3]==Word && checker[4]==Word && checker[5]==Word)
+        if (checker[3,0]==Word && checker[4,0]==Word && checker[5,0]==Word)
         { count++;}
-        if (checker[6]==Word && checker[7]==Word && checker[8]==Word)
+        if (checker[6,0]==Word && checker[7,0]==Word && checker[8,0]==Word)
         { count++;}
         return count;
     }
