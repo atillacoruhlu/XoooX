fa4e644ec822296fccec9d7445a245d92ce0a69d
atilla <atillacoruhlu@hotmail.com>
2020-08-24 20:04:29 +0300

    update gamemaster

--- XoooX/Assets/Scripts/GameMaster.cs
+++ XoooX/Assets/Scripts/GameMaster.cs
@@ -6,7 +6,7 @@ public class GameMaster : NetworkBehaviour
 {
 
     //Sırasıyla bütün hamleler.
-    public string[] Moves = new string[26] { "X", "O", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X" };
+    public string[] Moves = new string[27] { "X","X", "O", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X", "O", "X" };
 
     //Red Blue Green ve Yellow masaları için 3 boyutlu (İçinde X veya O olduğu["X","O"], Butonun adı["int"], Combo yapılıp yapılmadığı["", "F"]) bir array oluşturuyor.
     public string[,] Red = new string[9, 3] { { "", "1", "" }, { "", "2", "" }, { "", "3", "" }, { "", "6", "" }, { "", "7", "" }, { "", "8", "" }, { "", "11", "" }, { "", "12", "" }, { "", "13", "" } };
@@ -20,6 +20,7 @@ public class GameMaster : NetworkBehaviour
 
     public int Xcount = 0;
     public int Ocount = 0;
+    string renk2;
     private int planeToBuild;
     public static GameMaster instance;
     public Material[] materialXO;
@@ -55,8 +56,28 @@ public class GameMaster : NetworkBehaviour
 
     public void XOyaz(SyncListTuple _synctuple, string renk, string yazilacak, int yazilicakyer)
     {
-        Item item = new Item() { a = _synctuple[0].a, b = _synctuple[1].b, c = _synctuple[2].c };
+        Item item = new Item() { a = _synctuple[yazilicakyer].a, b = _synctuple[yazilicakyer].b, c = _synctuple[yazilicakyer].c };
         item.a = yazilacak;
+        switch (renk)
+        {
+            case "red":
+                syncRed[yazilicakyer] = item;
+                break;
+            case "green":
+                syncGreen[yazilicakyer] = item;
+                break;
+            case "yellow":
+                syncYellow[yazilicakyer] = item;
+                break;
+            case "blue":
+                syncBlue[yazilicakyer] = item;
+                break;
+        }
+    }
+    public void Fyaz(SyncListTuple _synctuple, string renk, string yazilacak, int yazilicakyer)
+    {
+        Item item = new Item() { a = _synctuple[yazilicakyer].a, b = _synctuple[yazilicakyer].b, c = _synctuple[yazilicakyer].c };
+        item.c = yazilacak;
         switch (renk)
         {
             case "red":
@@ -87,6 +108,14 @@ public class GameMaster : NetworkBehaviour
             Item item4 = new Item() { a = Blue[i, 0], b = Blue[i, 1], c = Blue[i, 2] };
             syncBlue.Add(item);
         }
+        Item item5 =new Item(){a="red",b="",c=""};
+        syncRed.Add(item5);
+        item5.a="green";
+        syncBlue.Add(item5);
+        item5.a="yellow";
+        syncYellow.Add(item5);
+        item5.a="blue";
+        syncBlue.Add(item5);
     }
 
     //ilk olarak gamemasterdan örnek oluşturuyoruz
@@ -101,6 +130,7 @@ public class GameMaster : NetworkBehaviour
         planeToBuild = 0;
         particle = SummonParticle;
         comboParticle = ComboParticle;
+        Debug.Log(syncRed[9].a);
     }
 
     void Update()
@@ -117,20 +147,14 @@ public class GameMaster : NetworkBehaviour
             planeToBuild = 2;
         }
         //comboları belirleyip puan değişkenine atıyoruz
-        Xcount += Check(Red, "X");
-        Xcount += Check(Green, "X");
-        Xcount += Check(Yellow, "X");
-        Xcount += Check(Blue, "X");
-        Ocount += Check(Red, "O");
-        Ocount += Check(Green, "O");
-        Ocount += Check(Yellow, "O");
-        Ocount += Check(Blue, "O");
-        if (syncRed[0].a == "X")
-        {
-            GameObject.Find("1").GetComponent<Renderer>().sharedMaterial = materialXO[1];
-        }
-
-        Debug.Log("Move Number: "+ MoveNumber);
+        Xcount += Check(syncRed, "X");
+        Xcount += Check(syncGreen, "X");
+        Xcount += Check(syncYellow, "X");
+        Xcount += Check(syncBlue, "X");
+        Ocount += Check(syncRed, "O");
+        Ocount += Check(syncGreen, "O");
+        Ocount += Check(syncYellow, "O");
+        Ocount += Check(syncBlue, "O");
     }
 
     public int GetPlaneBuild()
@@ -148,82 +172,92 @@ public class GameMaster : NetworkBehaviour
     //comboları karşılaştırıp kaçtane olduğunu değişkene atıyoruz
     //Yapılan hamleye göre renk ve material değişimini gerçekleştiriyoruz
     //comboların değiştiğini array öğelerinin 2.elemanına kaydediyoruz
-    public int Check(string[,] checker, string Word)
+    public int Check(SyncListTuple checker, string Word)
     {
 
-        int count = 0;
+        int count = 0; 
+        switch (checker[9].a)
+        {
+            case "red":renk2="red";
+                break;
+            case "blue":renk2="blue";
+                break;
+            case "green":renk2="green";
+                break;
+            case "yellow":renk2="yellow";
+                break;
+        }
 
-        if (checker[0, 0] == Word && checker[1, 0] == Word && checker[2, 0] == Word)
+        if (checker[0].a == Word && checker[1].a == Word && checker[2].a == Word)
         {
             count++;
-            if (Word == "X") { ChangePlaneColor(checker, 0, 1, 2, materialXO[3]); checker[0, 2] = "F"; checker[1, 2] = "F"; checker[2, 2] = "F"; } else { ChangePlaneColor(checker, 0, 1, 2, materialXO[4]); checker[0, 2] = "F"; checker[1, 2] = "F"; checker[2, 2] = "F"; }
+            if (Word == "X") { ChangePlaneColor(checker, 0, 1, 2, materialXO[3]); Fyaz(checker,renk2,"F",0);Fyaz(checker,renk2,"F",1);Fyaz(checker,renk2,"F",2); } else { ChangePlaneColor(checker, 0, 1, 2, materialXO[4]);Fyaz(checker,renk2,"F",0);Fyaz(checker,renk2,"F",1);Fyaz(checker,renk2,"F",2); }
         }
-        if (checker[0, 0] == Word && checker[3, 0] == Word && checker[6, 0] == Word)
+        if (checker[0].a == Word && checker[3].a == Word && checker[6].a == Word)
         {
             count++;
-            if (Word == "X") { ChangePlaneColor(checker, 0, 3, 6, materialXO[3]); checker[0, 2] = "F"; checker[3, 2] = "F"; checker[6, 2] = "F"; } else { ChangePlaneColor(checker, 0, 3, 6, materialXO[4]); checker[0, 2] = "F"; checker[3, 2] = "F"; checker[6, 2] = "F"; }
+            if (Word == "X") { ChangePlaneColor(checker, 0, 3, 6, materialXO[3]); Fyaz(checker,renk2,"F",0);Fyaz(checker,renk2,"F",3);Fyaz(checker,renk2,"F",6); } else { ChangePlaneColor(checker, 0, 3, 6, materialXO[4]); Fyaz(checker,renk2,"F",0);Fyaz(checker,renk2,"F",3);Fyaz(checker,renk2,"F",6);}
         }
-        if (checker[0, 0] == Word && checker[4, 0] == Word && checker[8, 0] == Word)
+        if (checker[0].a == Word && checker[4].a == Word && checker[8].a == Word)
         {
             count++;
-            if (Word == "X") { ChangePlaneColor(checker, 0, 4, 8, materialXO[3]); checker[0, 2] = "F"; checker[4, 2] = "F"; checker[8, 2] = "F"; } else { ChangePlaneColor(checker, 0, 4, 8, materialXO[4]); checker[0, 2] = "F"; checker[4, 2] = "F"; checker[8, 2] = "F"; }
+            if (Word == "X") { ChangePlaneColor(checker, 0, 4, 8, materialXO[3]); Fyaz(checker,renk2,"F",0);Fyaz(checker,renk2,"F",4);Fyaz(checker,renk2,"F",8); } else { ChangePlaneColor(checker, 0, 4, 8, materialXO[4]); Fyaz(checker,renk2,"F",0);Fyaz(checker,renk2,"F",4);Fyaz(checker,renk2,"F",8); }
         }
-        if (checker[1, 0] == Word && checker[4, 0] == Word && checker[7, 0] == Word)
+        if (checker[1].a == Word && checker[4].a == Word && checker[7].a == Word)
         {
             count++;
-            if (Word == "X") { ChangePlaneColor(checker, 1, 4, 7, materialXO[3]); checker[1, 2] = "F"; checker[4, 2] = "F"; checker[7, 2] = "F"; } else { ChangePlaneColor(checker, 1, 4, 7, materialXO[4]); checker[1, 2] = "F"; checker[4, 2] = "F"; checker[7, 2] = "F"; }
+            if (Word == "X") { ChangePlaneColor(checker, 1, 4, 7, materialXO[3]); Fyaz(checker,renk2,"F",1);Fyaz(checker,renk2,"F",4);Fyaz(checker,renk2,"F",7);} else { ChangePlaneColor(checker, 1, 4, 7, materialXO[4]);Fyaz(checker,renk2,"F",1);Fyaz(checker,renk2,"F",4);Fyaz(checker,renk2,"F",7);}
         }
-        if (checker[2, 0] == Word && checker[5, 0] == Word && checker[8, 0] == Word)
+        if (checker[2].a == Word && checker[5].a == Word && checker[8].a == Word)
         {
             count++;
-            if (Word == "X") { ChangePlaneColor(checker, 2, 5, 8, materialXO[3]); checker[2, 2] = "F"; checker[5, 2] = "F"; checker[8, 2] = "F"; } else { ChangePlaneColor(checker, 2, 5, 8, materialXO[4]); checker[2, 2] = "F"; checker[5, 2] = "F"; checker[8, 2] = "F"; }
+            if (Word == "X") { ChangePlaneColor(checker, 2, 5, 8, materialXO[3]); Fyaz(checker,renk2,"F",2);Fyaz(checker,renk2,"F",5);Fyaz(checker,renk2,"F",8); } else { ChangePlaneColor(checker, 2, 5, 8, materialXO[4]); Fyaz(checker,renk2,"F",2);Fyaz(checker,renk2,"F",5);Fyaz(checker,renk2,"F",8); }
         }
-        if (checker[2, 0] == Word && checker[4, 0] == Word && checker[6, 0] == Word)
+        if (checker[2].a == Word && checker[4].a == Word && checker[6].a == Word)
         {
             count++;
-            if (Word == "X") { ChangePlaneColor(checker, 2, 4, 6, materialXO[3]); checker[2, 2] = "F"; checker[4, 2] = "F"; checker[6, 2] = "F"; } else { ChangePlaneColor(checker, 2, 4, 6, materialXO[4]); checker[2, 2] = "F"; checker[4, 2] = "F"; checker[6, 2] = "F"; }
+            if (Word == "X") { ChangePlaneColor(checker, 2, 4, 6, materialXO[3]); Fyaz(checker,renk2,"F",2);Fyaz(checker,renk2,"F",4);Fyaz(checker,renk2,"F",6); } else { ChangePlaneColor(checker, 2, 4, 6, materialXO[4]); Fyaz(checker,renk2,"F",2);Fyaz(checker,renk2,"F",4);Fyaz(checker,renk2,"F",6); }
         }
-        if (checker[3, 0] == Word && checker[4, 0] == Word && checker[5, 0] == Word)
+        if (checker[3].a == Word && checker[4].a == Word && checker[5].a == Word)
         {
             count++;
-            if (Word == "X") { ChangePlaneColor(checker, 3, 4, 5, materialXO[3]); checker[3, 2] = "F"; checker[4, 2] = "F"; checker[5, 2] = "F"; } else { ChangePlaneColor(checker, 3, 4, 5, materialXO[4]); checker[3, 2] = "F"; checker[4, 2] = "F"; checker[5, 2] = "F"; }
+            if (Word == "X") { ChangePlaneColor(checker, 3, 4, 5, materialXO[3]); Fyaz(checker,renk2,"F",3);Fyaz(checker,renk2,"F",4);Fyaz(checker,renk2,"F",5);} else { ChangePlaneColor(checker, 3, 4, 5, materialXO[4]); Fyaz(checker,renk2,"F",1);Fyaz(checker,renk2,"F",4);Fyaz(checker,renk2,"F",7); }
         }
-        if (checker[6, 0] == Word && checker[7, 0] == Word && checker[8, 0] == Word)
+        if (checker[6].a == Word && checker[7].a == Word && checker[8].a == Word)
         {
             count++;
-            if (Word == "X") { ChangePlaneColor(checker, 6, 7, 8, materialXO[3]); checker[6, 2] = "F"; checker[7, 2] = "F"; checker[8, 2] = "F"; } else { ChangePlaneColor(checker, 6, 7, 8, materialXO[4]); checker[6, 2] = "F"; checker[7, 2] = "F"; checker[8, 2] = "F"; }
+            if (Word == "X") { ChangePlaneColor(checker, 6, 7, 8, materialXO[3]); Fyaz(checker,renk2,"F",6);Fyaz(checker,renk2,"F",7);Fyaz(checker,renk2,"F",8); } else { ChangePlaneColor(checker, 6, 7, 8, materialXO[4]); Fyaz(checker,renk2,"F",6);Fyaz(checker,renk2,"F",7);Fyaz(checker,renk2,"F",8); }
         }
         return count;
     }
     //arrayyin 2.öğesini denetleyerek değişiklik yapılmamışsa yapılmasını sağlıyoruz
     //objenin materilini değiştiriyoruz ve combo particallerini yaratıyoruz
-    public void ChangePlaneColor(string[,] rausch, int xeculus1, int xeculus2, int xeculus3, Material material_)
+    public void ChangePlaneColor(SyncListTuple rausch, int xeculus1, int xeculus2, int xeculus3, Material material_)
     {
-        if (rausch[xeculus1, 2] == "" || rausch[xeculus2, 2] == "" || rausch[xeculus3, 2] == "")
+        if (rausch[xeculus1].c == "" || rausch[xeculus2].c == "" || rausch[xeculus3].c == "")
         {
             //Her 3 Combo GameObjesinin materyalini ComboMateryali yapıyor.
-            GameObject.Find(rausch[xeculus1, 1]).GetComponent<Renderer>().sharedMaterial = material_;
-            GameObject.Find(rausch[xeculus2, 1]).GetComponent<Renderer>().sharedMaterial = material_;
-            GameObject.Find(rausch[xeculus3, 1]).GetComponent<Renderer>().sharedMaterial = material_;
+            GameObject.Find(rausch[xeculus1].b).GetComponent<Renderer>().sharedMaterial = material_;
+            GameObject.Find(rausch[xeculus2].b).GetComponent<Renderer>().sharedMaterial = material_;
+            GameObject.Find(rausch[xeculus3].b).GetComponent<Renderer>().sharedMaterial = material_;
 
             //Combo Particle'ını her 3 Combo GameObjesinde spawnlayıp, 2f sonra yok ediyor.
-            Destroy(Instantiate(ComboParticle, GameObject.Find(rausch[xeculus1, 1]).transform.position, GameObject.Find(rausch[xeculus1, 1]).transform.rotation), 2f);
-            Destroy(Instantiate(ComboParticle, GameObject.Find(rausch[xeculus2, 1]).transform.position, GameObject.Find(rausch[xeculus2, 1]).transform.rotation), 2f);
-            Destroy(Instantiate(ComboParticle, GameObject.Find(rausch[xeculus3, 1]).transform.position, GameObject.Find(rausch[xeculus3, 1]).transform.rotation), 2f);
+            Destroy(Instantiate(ComboParticle, GameObject.Find(rausch[xeculus1].b).transform.position, GameObject.Find(rausch[xeculus1].b).transform.rotation), 2f);
+            Destroy(Instantiate(ComboParticle, GameObject.Find(rausch[xeculus2].b).transform.position, GameObject.Find(rausch[xeculus2].b).transform.rotation), 2f);
+            Destroy(Instantiate(ComboParticle, GameObject.Find(rausch[xeculus3].b).transform.position, GameObject.Find(rausch[xeculus3].b).transform.rotation), 2f);
         }
 
     }
     //
     public void changeArray(string ButtonName)
-    {
-        GameObject.Find(ButtonName).GetComponent<Renderer>().sharedMaterial = materialXO[1];
+    {   if(Moves[MoveNumber]=="X"){GameObject.Find(ButtonName).GetComponent<Renderer>().sharedMaterial = materialXO[1];}else {GameObject.Find(ButtonName).GetComponent<Renderer>().sharedMaterial = materialXO[2];}
+        
+        
         switch (ButtonName)
         {
             //bastığımız butona göre array öğelerinin 0. kısmına moves arrayindeki hamleyi yazıyoruz
             case "1":
                 XOyaz(syncRed, "red", Moves[MoveNumber], 0);
-
-
                 break;
             case "2":
                 XOyaz(syncRed, "red", Moves[MoveNumber], 1);
