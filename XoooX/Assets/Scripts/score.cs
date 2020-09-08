351a29d432932ca732b8bc1d45ffa8467d2c4e5a
Özgür Özbek <ozgurozbek1@yandex.com>
2020-09-08 06:03:31 +0300

    Networking
    Mirror Bulk is decreased.
    Multiplayer turns logic added.
    Some scripts are changed for Mirror to perform better.
    Some scripts are moved to main project.
    Some prefabs are altered for them to work better with Sync

--- XoooX/Assets/Scripts/score.cs
+++ XoooX/Assets/Scripts/score.cs
@@ -3,6 +3,6 @@ using Mirror;
 //gamemasterdaki değişkenlerden puanları ekrana yazıyoruz
 public class Score : NetworkBehaviour {
     void Update () {
-        GetComponent<TextMesh> ().text = "X :" + GameMaster.instance.Xcount + " O :" + GameMaster.instance.Ocount;
+        GetComponent<TextMesh> ().text = "X " + GameMaster.instance.Xcount + "-" + GameMaster.instance.Ocount + " O";
     }
 }
