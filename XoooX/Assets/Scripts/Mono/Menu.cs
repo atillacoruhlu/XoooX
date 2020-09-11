6739a2b1936f71491426b1e68f25f0dbed1537fe
Özgür Özbek <ozgurozbek1@yandex.com>
2020-09-12 01:59:17 +0300

    Patch
    PlayerPref Table
    Thesis
    Cleanup
    Sounds
    Tutorial & Info Scenes

---
+++ XoooX/Assets/Scripts/Mono/Menu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    public Camera menuCamera;
+
+    private void Awake () {
+        Instantiate (menuCamera, new Vector3 (0, 0, 0), Quaternion.identity);
+    }
+    public void PlayButton () {
+        Initiate.Fade ("Offline_Scene", Color.black, 2); //Loads Mirror
+    }
+
+    public void PracticeButton () {
+        Initiate.Fade ("MiniMax_Scene", Color.black, 2); //Loads AI Scene
+    }
+
+    public void TutorialButton () {
+        Initiate.Fade ("Tutorial_Scene", Color.black, 2); //Loads tutorial
+    }
+
+    public void InfoButton () {
+        Initiate.Fade ("Info_Scene", Color.black, 2); //Loads credits
+    }
+
+    public void EasterEggButton () {
+        if (PlayerPrefs.GetInt ("Egg", 1) == 0) {
+            PlayerPrefs.SetInt ("Egg", 1); // Off
+        } else {
+            PlayerPrefs.SetInt ("Egg", 0); // On
+        }
+    }
+
+    public void ReturnButton () {
+        Initiate.Fade ("Menu_Scene", Color.black, 2); //Loads menu
+    }
+
+    public void ExitGame () {
+        Application.Quit (); //Stops existing
+    }
+}
---
+++ XoooX/Assets/Scripts/Mono/TableMaterialController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TableMaterialController : MonoBehaviour {
+
+    public Material eggMaterial;
+    // Start is called before the first frame update
+    private void Awake () {
+        if (PlayerPrefs.GetInt ("Egg", 1) == 0) {
+            //Default is OFF, but if it is ON...
+            this.GetComponent<Renderer> ().sharedMaterial = eggMaterial;
+        }
+    }
+}
